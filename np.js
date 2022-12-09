516a63ade92021999b757b98b7e9f7d47eb86136
bem130 <haruki070617@gmail.com>
2022-12-09 21:24:49 +0900

    change a little

--- np.js
+++ np.js
@@ -10,16 +10,39 @@ class NPdata {
 
 class SolveNP {
     constructor(data) {
+        // prepare
         this.sboard = new Array(9);
         for (let i=0;i<9;i++) {
             this.sboard[i] = new Array(9).fill(null);
         }
+        // init
         for (let i=0;i<9;i++) {
             for (let j=0;j<9;j++) {
                 this.sboard[i][j] = [data.getCell(i+1,j+1)];
                 for (let k=0;k<9;k++) {
                     this.sboard[i][j].push(true);
                 }
+            }
+        }
+        // the first condition
+        for (let i=0;i<9;i++) {
+            for (let j=0;j<9;j++) {
+                if (this.sboard[i][j][0]!=null) {
+                    for (let k=1;k<10;k++) {
+                        if (this.sboard[i][j][0]!=k) {
+                            this.sboard[i][j][k] = false;
+                        }
+                    }
+                }
+            }
+        }
+        console.table(this.sboard);
+        this.updateStat();
+    }
+    updateStat() {
+        for (let i=0;i<9;i++) {
+            for (let j=0;j<9;j++) {
+                // conditions
             }
         }
         console.table(this.sboard);
