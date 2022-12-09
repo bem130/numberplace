5a34e0b42dcd1ccc83547c82c5fd771c36b0fafd
bem130 <haruki070617@gmail.com>
2022-12-09 19:34:11 +0900

    started making the code to solve

--- np.js
+++ np.js
@@ -10,5 +10,18 @@ class NPdata {
 
 class SolveNP {
     constructor(data) {
+        this.sboard = new Array(9);
+        for (let i=0;i<9;i++) {
+            this.sboard[i] = new Array(9).fill(null);
+        }
+        for (let i=0;i<9;i++) {
+            for (let j=0;j<9;j++) {
+                this.sboard[i][j] = [data.getCell(i+1,j+1)];
+                for (let k=0;k<9;k++) {
+                    this.sboard[i][j].push(true);
+                }
+            }
+        }
+        console.table(this.sboard);
     }
 }
